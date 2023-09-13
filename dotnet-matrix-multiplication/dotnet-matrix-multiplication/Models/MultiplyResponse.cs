1474c8488bb870759cf89f21ae5fa54fcb86b9f4
nelsonfr <nelsonfabianrios@hotmail.com>
2023-09-13 18:58:48 -0500

    added cors and MultiplyResponse

---
+++ dotnet-matrix-multiplication/dotnet-matrix-multiplication/Models/MultiplyResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace dotnet_matrix_multiplication.Models
+{
+	public class MultiplyResponse
+	{
+		public Matrix MatrixA { get; set; }
+		public Matrix MatrixB { get; set; }	
+		public Matrix Result { get; set; }
+
+        public MultiplyResponse(Matrix matrixA, Matrix matrixB, Matrix result)
+        {
+            MatrixA = matrixA;
+			MatrixB = matrixB;
+			Result = result;
+        }
+    }
+}
--- dotnet-matrix-multiplication/dotnet-matrix-multiplication/Program.cs
+++ dotnet-matrix-multiplication/dotnet-matrix-multiplication/Program.cs
@@ -2,9 +2,20 @@ using dotnet_matrix_multiplication.Middleware;
 using dotnet_matrix_multiplication.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+var policy = "AllowSpecificOrigins";
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(name: policy,
+					  policy =>
+					  {
+						  policy.WithOrigins("http://localhost:4200");
+						  policy.AllowAnyHeader();
+						  policy.AllowAnyMethod();
+					  });
 
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,6 +31,8 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
+
+app.UseCors(policy);
 
 app.UseAuthorization();
 
